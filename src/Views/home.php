2e086e6c3c095d0f6837625fffcd1fba29547a8d
seb-since <sphilippe@sinceandco.fr>
2025-07-18 07:09:11 +0200

    Change title

--- src/Views/home.php
+++ src/Views/home.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
-  <title>Fun File Uploader</title>
+  <title>File Uploader</title>
   <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
